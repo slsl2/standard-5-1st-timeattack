9643d23c2616f97a3880dab78380a29bc7538433
slsl2 <kimminji7153@gmail.com>
2024-05-20 15:50:35 +0900

    calculator 제출

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
+import React from "react";
+import { useState } from "react";
+
 export default function App() {
+  const [number, setNumber] = useState("");
+  const [result, setResult] = useState(0);
+
+  const handleInput = (event) => {
+    setNumber(event.target.value);
+  };
+
+  const addNumber = () => {
+    setResult(result + Number(number));
+    setNumber("");
+  };
+
+  const minusNumber = () => {
+    setResult(result - Number(number));
+    setNumber("");
+  };
+
+  const resetResult = () => {
+    setResult(0);
+    setNumber("");
+  };
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input type="number" onChange={handleInput} value={number}></input>{" "}
+        만큼을 <button onClick={addNumber}>더할게요</button>{" "}
+        <button onClick={minusNumber}>뺄게요</button>
+        <button onClick={resetResult}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{result}</p>
       </div>
     </div>
   );
